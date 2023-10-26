7266259a7d5d5b3d821c90e192ba98ddaf0daedc
B C Samrudh <samrudhbc@gmail.com>
2023-10-26 10:11:00 +0530

    Removed Unwanted Files

--- src/components/empty-placeholder.jsx
+++ src/components/empty-placeholder.jsx
@@ -1,15 +1,4 @@
 import { Button } from "@/components/ui/button"
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import { VscSymbolProperty } from "react-icons/vsc"
 
 export function EmptyPlaceholder() {
--- src/components/mortgage-calc.jsx
+++ src/components/mortgage-calc.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 
 const MortgageCalculator = () => {
