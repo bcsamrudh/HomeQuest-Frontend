c7cb0a230cb4a58764f0ff3712542b9bd09da4f4
B C Samrudh <samrudhbc@gmail.com>
2023-11-01 08:44:11 +0530

    Added User Authentication

--- src/app/auth/page.js
+++ src/app/auth/page.js
@@ -1,3 +1,4 @@
+'use client'
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
@@ -7,43 +8,142 @@ import {
   TabsList,
   TabsTrigger,
 } from "@/components/ui/tabs"
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from 'src/redux/user/userSlice';
 
 
 function Register(){
+    const [formData, setFormData] = useState({});
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const navigate = useRouter();
+    const handleChange = (e) => {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.value,
+      });
+    };
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      try {
+        setLoading(true);
+        const res = await fetch('http://localhost:3000/api/auth/signup', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData),
+        });
+        const data = await res.json();
+        console.log(data);
+        if (data.success === false) {
+          setLoading(false);
+          setError(data.message);
+          return;
+        }
+        setLoading(false);
+        setError(null);
+        navigate.push('/');
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+        setError(error.message);
+      }
+    };
       return (
         <div id="register" className="w-[500px] p-4">
-          <h1 className="text-3xl mb-1 font-bold text-regparacolor w-[500px]">Create An Account</h1>
-          <p className="text-lg mb-4 w-[193px]">Provide your details.</p>
-          <Label htmlFor="name" className="mb-2 text-lg">Your name</Label>
-          <Input id="name" type="text" className="mb-2 w-[397px] h-[47px] border border-regparacolor" placeholder="First Last" />
-          <Label htmlFor="name" className="mb-2 text-lg">Email</Label>
-          <Input type="email"  className="mb-2 w-[397px] h-[47px] border border-regparacolor" placeholder="you@gmail.com" />
-          <Label htmlFor="name" className="mb-2 text-lg ">Password</Label>
-          <Input type="password" className="mb-2 w-[397px] h-[47px] border border-regparacolor" placeholder="Password" />
-          <Button className="bg-myblue mb-2 rounded-lg w-[397px] h-[47px]">Create an Account</Button>
-          <Button className="bg-regparacolor mb-2 rounded-lg w-[397px] h-[47px] hover:bg-regparacolor text-white"> 
-          <img
-          src="\assests\Google.svg"
-          alt="Image"/>
-          &nbsp;&nbsp;or register with Google</Button>
+          <form onSubmit={handleSubmit}>
+            <h1 className="text-3xl mb-1 font-bold text-regparacolor w-[500px]">Create An Account</h1>
+            <p className="text-lg mb-4 w-[193px]">Provide your details.</p>
+            <Label htmlFor="name" className="mb-2 text-lg">Your username</Label>
+            <Input id="username"
+            type="text"
+            className="mb-2 w-[397px] h-[47px] border border-regparacolor"
+            placeholder="First Last"
+            onChange={handleChange}/>
+            <Label htmlFor="name" className="mb-2 text-lg">Email</Label>
+            <Input type="email"
+            id="email"
+            className="mb-2 w-[397px] h-[47px] border border-regparacolor"
+            placeholder="you@gmail.com"
+            onChange={handleChange}/>
+            <Label htmlFor="name" className="mb-2 text-lg ">Password</Label>
+            <Input type="password"
+            id="password"
+            className="mb-2 w-[397px] h-[47px] border border-regparacolor"
+            placeholder="Password"
+            onChange={handleChange}/>
+            <Button disabled={loading}
+            className="bg-myblue mb-2 rounded-lg w-[397px] h-[47px]">
+            {loading ? 'Loading...' : 'Create an Account'}
+            </Button>
+          </form>
           </div>
       )
 }
 
 function Login(){
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
+  const navigate = useRouter();
+  const dispatch = useDispatch();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(signInStart());
+      const res = await fetch('http://localhost:3000/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
+        return;
+      }
+      dispatch(signInSuccess(data));
+      navigate.push('/dashboard');
+    } catch (error) {
+      dispatch(signInFailure(error.message));
+  }
+};
   return (
     <div id="login" className="w-[500px] p-4">
-      <h1 className="text-3xl mb-5 font-bold text-regparacolor w-[500px]">Welcome Back!</h1>
-      <Label htmlFor="name" className="mb-2 text-lg">Email</Label>
-      <Input type="email"  className="mb-2 w-[397px] h-[47px] border border-regparacolor" placeholder="you@gmail.com" />
-      <Label htmlFor="name" className="mb-2 text-lg ">Password</Label>
-      <Input type="password" className="mb-2 w-[397px] h-[47px] border border-regparacolor" placeholder="Password" />
-      <Button className="bg-myblue mb-2 rounded-lg w-[397px] h-[47px]">Sign In</Button>
-      <Button className="bg-regparacolor mb-2 rounded-lg w-[397px] h-[47px] hover:bg-regparacolor text-white"> 
-          <img
-          src="\assests\Google.svg"
-          alt="Image"/>
-          &nbsp;&nbsp;or continue with Google</Button>
+      <form onSubmit={handleSubmit}>
+        <h1 className="text-3xl mb-5 font-bold text-regparacolor w-[500px]">Welcome Back!</h1>
+        <Label htmlFor="name" className="mb-2 text-lg">Email</Label>
+        <Input type="email"
+        id="email"  
+        className="mb-2 w-[397px] h-[47px] border border-regparacolor" 
+        placeholder="you@gmail.com" 
+        onChange={handleChange}/>
+        <Label htmlFor="name" className="mb-2 text-lg ">Password</Label>
+        <Input type="password"
+        id="password"
+        className="mb-2 w-[397px] h-[47px] border border-regparacolor"
+        placeholder="Password"
+        onChange={handleChange}/>
+        <Button disabled={loading} 
+        className="bg-myblue mb-2 rounded-lg w-[397px] h-[47px]">
+        {loading ? 'Loading...' : 'Sign In'}
+        </Button>
+      </form>
+      {error && <p>{error}</p>}
       </div>
   )
 }
@@ -70,9 +170,7 @@ export default function Home() {
         <TabsTrigger value="password">Login</TabsTrigger>
       </TabsList>
       <TabsContent value="account">
-      <form className='flex flex-col gap-4'> 
         <Register/>
-      </form>
       </TabsContent>
       <TabsContent value="password">
         <Login/>
@@ -82,4 +180,4 @@ export default function Home() {
       </div>
       </div>
     )
-}
+  }
--- src/components/ui/menubar.jsx
+++ src/components/ui/menubar.jsx
@@ -4,28 +4,67 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-import { VscSignIn } from "react-icons/vsc"
+import { VscSignIn } from "react-icons/vsc";
+import { useRouter } from "next/navigation";
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  signOutUserStart,
+} from 'src/redux/user/userSlice';
 
 export function UserNav() {
-  const currentUser = null
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch('http://localhost:3000/api/auth/signout');
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+      router.push('/auth')
+    } catch (error) {
+      dispatch(deleteUserFailure(error));
+    }
+  };
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <div className="flex items-center">
-             <div className="h-6 w-6 rounded-full flex items-center justify-center text-white">
-        </div>
         {currentUser?
-        <span className="ml-auto">{currentUser.username}</span>:
+        <>
+          <div className="h-6 w-6 rounded-full flex items-center justify-center mr-2">
+              <img
+              src={currentUser.avatar} 
+              alt="Profile"
+              className="rounded-full"
+              />
+          </div>
+          <span className="ml-auto text-bold">{currentUser.username}</span>
+        </>:
         <span className="ml-auto">Guest</span>
         }
         </div>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56" align="end" forceMount>
-        <DropdownMenuItem className="text-red-500" onClick={() => window.location.href = "./auth"}>
-        <VscSignIn className="mr-2 h-4 w-4" />
-          Log out
-        </DropdownMenuItem>
-      </DropdownMenuContent>
+      {currentUser && 
+      <>
+        <DropdownMenuContent className="w-56" align="end" forceMount>
+          <DropdownMenuItem className="text-red-500" onClick={handleSignOut}>
+          <VscSignIn className="mr-2 h-4 w-4" />
+            Log out
+          </DropdownMenuItem>:
+        <></>
+        </DropdownMenuContent>
+      </>}
     </DropdownMenu>
   )
 }
