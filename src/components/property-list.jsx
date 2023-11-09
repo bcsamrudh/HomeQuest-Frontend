a87a3cc13bcba6978b83e28c092a1435956a357f
B C Samrudh <samrudhbc@gmail.com>
2023-11-09 12:27:53 +0530

    Updated the UI

--- src/components/property-list.jsx
+++ src/components/property-list.jsx
@@ -2,12 +2,6 @@
 import { PlusCircledIcon } from "@radix-ui/react-icons"
 import { Button } from "@/components/ui/button"
 import SearchBar from '@/components/searchbar'
-import {
-    Tabs,
-    TabsContent,
-    TabsList,
-    TabsTrigger,
-  } from "@/components/ui/tabs"
 import { EmptyPlaceholder } from "@/components/empty-placeholder"
 
 export default function PropertyList(){
@@ -17,38 +11,16 @@ export default function PropertyList(){
         <div className="pb-4">
         <div className="flex space-x-4">
           <SearchBar />
-          {/* <Filter propertyTypes={options}/> */}
-        </div>
-        </div>
-          <Tabs defaultValue="music" className="h-full space-y-6">
-            <div className="space-between flex items-center">
-            <TabsList>
-                <TabsTrigger value="popular">Popular</TabsTrigger>
-                <TabsTrigger value="newly-added">
-                   Newly Added
-                </TabsTrigger>
-              </TabsList>
-              <div className="ml-auto mr-4">
+          <div className="ml-auto mr-4">
                 <Button>
                   <PlusCircledIcon className="mr-2 h-4 w-4" />
                   Add Property
                 </Button>
-              </div>
-            </div>
-            <TabsContent
-              value="popular"
-              className="h-full flex-col border-none p-0 data-[state=active]:flex"
-            >
-            <EmptyPlaceholder />
-            </TabsContent>
-            <TabsContent
-              value="newly-added"
-              className="h-full flex-col border-none p-0 data-[state=active]:flex"
-            >
-              <EmptyPlaceholder />
-            </TabsContent>
-          </Tabs>
+          </div>
         </div>
-      </div>
+        </div>
+          <EmptyPlaceholder/>
+        </div>
+        </div>
     )
 }
